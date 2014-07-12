fde2551897046e805e495ee88d57c5ecd8d0eb43
NigelVH <nigelvh@users.noreply.github.com>
2014-07-11 18:27:35 -0700

    Cleaned cruft and added comments to utilities/ascii_hex_to_bin.c and utilities/distribution.c

--- utilities/ascii_hex_to_bin.c
+++ utilities/ascii_hex_to_bin.c
@@ -1,28 +1,11 @@
+/* (c) 2014 Nigel Vander Houwen */
 /* Compile: gcc -Wall -O ascii_hex_to_bin.c -o ascii_hex_to_bin */
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdint.h>
 
-uint8_t read_uint8(int *reached_eof)
-{
-        int i, c;
-        uint8_t value = 0;
-
-        for (i = 0; i < 8; i++) {
-                c = fgetc(stdin);
-                if (c == EOF) {
-                        *reached_eof = 1;
-                        break;
-                }
-                c = (c == '0') ? 0 : 1;
-                value = (value << 1) | c;
-        }
-        return value;
-}
-
-unsigned char hexval(unsigned char c)
-{
+// Takes in a char 0-9,A-F,a-f and returns the int value 0-15
+unsigned char hexval(unsigned char c){
 	if('0' <= c && c <= '9'){
 		return c - '0';
 	}else if('A' <= c && c <= 'F'){
@@ -35,21 +18,23 @@ unsigned char hexval(unsigned char c)
 	}
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 	signed char byte1;
 	signed char byte2;
 
 	while(1){
+		// Grab two bytes from STDIN, and make sure they're not EOFs
 		byte1 = fgetc(stdin);
 		if(byte1 == EOF) break;
 		byte2 = fgetc(stdin);
 		if(byte2 == EOF) break;
 
+		// Grab the int value of each of the two chars, and add them together to get a byte's worth of data
 		unsigned char num = (hexval(byte1) << 4) + hexval(byte2);
 
+		// Write the raw byte out to stdout
 		fwrite(&num, sizeof(num), 1, stdout);
 	}
 
-        return 0;
+    return 0;
 }
--- utilities/distribution.c
+++ utilities/distribution.c
@@ -1,3 +1,4 @@
+/* (c) 2014 Nigel Vander Houwen */
 /* Compile: gcc -Wall -O distribution.c -o distribution                        */
 /* Takes in a file as an argument, reads through the file, and outputs a space */
 /* separated list of the total count of each character value from 0-255        */
@@ -6,8 +7,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
+	// Grab a file pointer and open the file in read only binary mode.
 	FILE *fp;
-
 	fp  = fopen(argv[1], "rb");
 
 	// Set up a couple variables
@@ -17,14 +18,17 @@ int main(int argc, char *argv[]){
 
 	// The device is now open, lets try reading from it.
 	while(1){
+		// Grab a byte from the file
 		byte1 = fgetc(fp);
 
+		// If the byte is an EOF, prepare to quit, else increment the array for that bin.
 		if(byte1 == EOF){
 			eof_reached = 1;
 		}else{
 			distribution[(unsigned char)byte1]++;
 		}
 
+		// At the end of the file, lets output the counts from each of the bins.
 		if(eof_reached){
 			for(int i = 0; i < 256; i++){
 				printf("%lu ", distribution[i]);
